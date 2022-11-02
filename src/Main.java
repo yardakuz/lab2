89627b67cb20ed3af0b75635c93ee102b8f82ab7
yarda <yardakuz@gmail.com>
2022-11-02 23:15:26 +0400

    2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@ import functions.*;
 public class Main {
     public static void main(String[] args)
     {
-        FunctionPoint point = new FunctionPoint(3, 2);
-        FunctionPoint point2 = new FunctionPoint(point);
-        System.out.println(point2.getX());
-        System.out.println('\n');
-        System.out.println(point2.getY());
+        TabulatedFunction points = new TabulatedFunction(0, 5, 6);
+        points.deletePoint(0);
+        points.print();
+
     }
 }
--- src/functions/TabulatedFunction.java
+++ src/functions/TabulatedFunction.java
@@ -3,9 +3,9 @@ package functions;
 public class TabulatedFunction {
     private FunctionPoint[] points;         //массив точек
 
-    private int NumberPoints;
+    private int NumberPoints;           //кол-во точек в массиве
 
-    TabulatedFunction(double leftX, double rightX, int pointsCount)         //конструктор для заданных границ x и количества точек
+    public TabulatedFunction(double leftX, double rightX, int pointsCount)         //конструктор для заданных границ x и количества точек
     {
         this.points = new FunctionPoint[pointsCount + 10];
         double len = (rightX - leftX) / (pointsCount - 1);
@@ -16,7 +16,7 @@ public class TabulatedFunction {
         this.NumberPoints = pointsCount;
     }
 
-    TabulatedFunction(double leftX, double rightX, double[] values)             //конструктор для заданных границ x и массива из значений y
+    public TabulatedFunction(double leftX, double rightX, double[] values)             //конструктор для заданных границ x и массива из значений y
     {
         this.points = new FunctionPoint[values.length + 10];
         double len = (rightX - leftX)/ (values.length - 1);
@@ -50,7 +50,6 @@ public class TabulatedFunction {
         a = (y2 - y1)/(x2 - x1);
         b = y1 - x1*a;
         return x * a + b;
-
     }
 
     public int getPointsCount()        //метод, возвращающий количество точек
@@ -107,16 +106,56 @@ public class TabulatedFunction {
 
     public void deletePoint(int index)      //метод, удаляющий заданную точку
     {
-
+        if (index < 0 || index > NumberPoints) return;
+        System.arraycopy(points, index + 1, points, index, this.points.length - index - 1);
+        --NumberPoints;
     }
 
     public void addPoint(FunctionPoint point)       //метод, добавляющий новую точку
     {
+        int index = 0;
+        if(point.getX() > this.points[NumberPoints - 1].getX())
+        {
+            index = NumberPoints + 1;
+        }
+        else
+        {
+            while (this.points[index].getX() < point.getX()) ++index;
+        }
+
+        if (index < NumberPoints)
+        {
+            if (NumberPoints <= this.points.length)
+            {
+                FunctionPoint[] tmp = new FunctionPoint[NumberPoints];
+                System.arraycopy(points, 0, tmp, 0, tmp.length);
+                this.points = new FunctionPoint[NumberPoints + 1];
+                ++NumberPoints;
+                System.arraycopy(tmp, 0, points, 0, index);
+                this.points[index] = point;
+            }
+            else
+            {
+                System.arraycopy(points, index, points, index + 1, NumberPoints - index);
+                this.points[index] = point;
+            }
+        }
+        else
+        {
+            FunctionPoint[] tmp = new FunctionPoint[getPointsCount()];
+            System.arraycopy(points,0,tmp,0,tmp.length);
+            this.points = new FunctionPoint[getPointsCount() + 1];
+            NumberPoints++;
+            System.arraycopy(tmp,0,points,0,tmp.length);
+            this.points[NumberPoints-1] = point;
+
+        }
 
     }
 
-
-
-
-
+    public void print() {
+        for (int i = 0; i < NumberPoints; i++) {
+            System.out.println("(" + points[i].getX() + " ; " + points[i].getY() + ")");
+        }
+    }
 }
